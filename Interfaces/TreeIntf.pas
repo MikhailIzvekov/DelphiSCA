b0f210e2d6bfc52b8001a65947489d58e988fc67
MikhailIzvekov <mihail@izvekov.com>
2017-03-08 10:55:41 +0400

    Forget the GUID

--- Interfaces/TreeIntf.pas
+++ Interfaces/TreeIntf.pas
@@ -7,6 +7,7 @@ uses
 
 type
   IEquivalenceChecker = interface
+  ['{BC1CA693-9FB1-4C89-A8DD-ED5EA89DE312}']
     function Equal(const ANode1: TSyntaxNode; const ANode2: TSyntaxNode): Boolean;
   end;
 
